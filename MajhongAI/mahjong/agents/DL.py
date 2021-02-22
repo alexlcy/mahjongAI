cdbd6c89706a4b03ff47a8f7f79fd442daea277c
Alex Lo <alexlocheungyu@gmail.com.hk>
2021-02-22 23:25:45 +0800

    Adding the discard logic to the Deeplearning agent

--- MajhongAI/mahjong/agents/DL.py
+++ MajhongAI/mahjong/agents/DL.py
@@ -16,7 +16,7 @@ from mahjong.consts import COMMAND
 
 class DeepLearningAgent(object):
     def __init__(self, player_id: int):
-        self.name = 'rule'
+        self.name = 'deeplearning'
         self.__player_id = player_id
         self.state_tensor = np.zeros((9, 8, 10))
 
@@ -38,11 +38,12 @@ class DeepLearningAgent(object):
         # Exception handling
         if not legal_actions or len(legal_actions) == 0:
             return
+
         if len(legal_actions) == 1:
             player['choice'] = legal_actions[0]
             return
 
-        # 选缺 process
+        # Step 1: 选缺 process (Only trigger once in a round)
         colors = [0] * 3
         for card in player['hands']:
             colors[math.floor(card/10)] += 1
@@ -50,25 +51,19 @@ class DeepLearningAgent(object):
             player['choice'] = COMMAND.COLOR.value + colors.index(min(colors))
             return
 
-        # Choose which Action
+        # Step 2: Choose which Action
         player['choice'] = max(legal_actions)
 
-        # Choose which one to discard
+        # Step 3: Choose which one to discard
         if player['choice'] < 100:
-            cards = [0] * 30
-            for card in player['hands']:
-                cards[card] += 1
-            for card in range(30):
-                if cards[card] == 1:
-                    player['choice'] = card
-                    return
-            for card in range(30):
-                if cards[card] == 2:
-                    player['choice'] = card
-                    return
-            player['choice'] = random.choice(legal_actions)
-        # player['choice'] = random.choice(player['legal_actions'])
 
+            # Call discard function to discard a tile
+            discard_tile = self.decide_discard(player)
+            if discard_tile is not None:
+                player['choice'] = discard_tile
+                return
+
+            player['choice'] = random.choice(legal_actions)
 
     def decide_kong(self):
         # Decide whether to make a Kong
@@ -85,24 +80,93 @@ class DeepLearningAgent(object):
         return whether_pong, score
 
     def decide_win(self):
-        # Later will implementa decide win model
+        # Later will implement decide win model
         raise NotImplementedError
 
-    def decide_discard(self):
+    def decide_discard(self, player):
         """
-        If there are no 缺 in the tile, then we will call the decide_tile_discard
+        The tile is discarded based on the below sequence
+        1. Discard color tile if there exist
+        2. Discard based on model suggestion
+        3. Discard based on naive rules
 
+        Returns:
+            discard_tile: The tile want to discard
+        """
+
+        # Priority 1: Discard based on color
+        color_discard_tile = self.decide_discard_by_color()
+        if color_discard_tile is not None:
+            return color_discard_tile
+
+        # Priority 2: Discard based on AI model
+        ai_discard_tile = self.decide_discard_by_AI()
+        if ai_discard_tile in player['hands']:
+            return ai_discard_tile
+
+        # Priority 3: Discard based on naive rule
+        return self.decide_discard_by_rule
+
+    def decide_discard_by_AI(self):
+        """
+        Call the discard model and return the tile that we shoudl discard
         Returns:
         """
         raise NotImplementedError
 
-    def decide_tile_discard(self):
+    def decide_discard_by_color(self, player):
         """
+        If there exist a tile that belongs to the color (缺), the player should
+        discard it before it discard other tile.
 
+        Args:
+            player ():
 
         Returns:
-        """
-        raise NotImplementedError
 
-        raise NotImplementedError
+        """
+        # Discard based on color (缺)
+        color = player['color']
+
+        # Discard if the there are only one 缺 tile
+        for tile_num in range(1, 10):
+            if player['hands'][color + tile_num] == 1:
+                return color + tile_num
+
+        # Discard if the there are two 缺 tile
+        for tile_num in range(1, 10):
+            if player['hands'][color + tile_num] == 2:
+                return color + tile_num
+
+        # Discard if the there are only three tile
+        for tile_num in range(1, 10):
+            if player['hands'][color + tile_num] == 2:
+                return color + tile_num
+
+        # If no 缺 tile available, then skip the 缺 discard function
+        return None
+
+
+    def decide_discard_by_rule(self,player):
+        """
+        When there are no valid decision made by AI and no discard based on color.
+        The discard will be conducted using the same naive rule as the rule agent.
+
+        Args:
+            player ():
+
+        Returns:
+
+        """
+        # Discard based on rule
+        cards = [0] * 30
+        for card in player['hands']:
+            cards[card] += 1
+
+        for card in range(30):
+            if cards[card] == 1:
+                return card
+        for card in range(30):
+            if cards[card] == 2:
+                return card
 
