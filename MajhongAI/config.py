668385463a02d51f5e11b57f9b8e0b7cfd7ab091
jaysonph <iamjaysonph@gmail.com>
2021-04-06 14:59:46 +0800

    Changed to new discard model config

--- MajhongAI/config.py
+++ MajhongAI/config.py
@@ -1,6 +1,6 @@
 # ==================================== Model weight ====================================
 pong_weight_path = './mahjong/models/weights/pong/pong-ep3-val_acc_0.8889-val_f1_0.9078.pth'
 kong_weight_path = './mahjong/models/weights/kong/kong-ep55-val_acc_0.8862-val_f1_0.9218.pth'
-discard_weight_path = './mahjong/models/weights/discard/discard-ep10-val_acc_0.6793-val_f1_0.6787.pth'
+discard_weight_path = './mahjong/models/weights/discard/discard-ep17-val_acc_0.7356-val_f1_0.7356.pth'
 
 log_dir = './runs/run1900'
